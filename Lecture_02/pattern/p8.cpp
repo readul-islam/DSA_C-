318fa5d7700241f1cf2977e067de4e13b472d37e
readul-islam <developer.readul@gmail.com>
2024-08-15 16:35:56 +0600

    pattern practice

---
+++ Lecture_02/pattern/p8.cpp
@@ -0,0 +1,24 @@
+/*
+A A A A
+B B B B
+C C C C
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int row = 1;
+    while(row<=n){
+        int col =1;
+        while(col<=n){
+            char ch = 'A' + row -1;
+            cout << ch<<" ";
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
