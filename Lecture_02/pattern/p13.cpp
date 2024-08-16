560e8fd1688ab0a4a8b53251c550804a68db6137
readul-islam <developer.readul@gmail.com>
2024-08-16 15:33:52 +0600

    pattern practice

---
+++ Lecture_02/pattern/p13.cpp
@@ -0,0 +1,30 @@
+/*
+   *
+  **
+ ***
+****
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, row = 1;
+    cin>> n;
+    while(row<=n){
+        int space = n -row;
+        while(space){
+            cout << " " ;
+            space--;
+        }
+        int col = 1;
+        while(col<=row){
+            cout << "*";
+            col++;
+        }
+        cout << endl;
+        row++;
+
+    }
+
+
+}
