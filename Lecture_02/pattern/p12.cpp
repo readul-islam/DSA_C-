6c7693b15f6cb2f88512a92b3abd64522cf7c720
readul-islam <developer.readul@gmail.com>
2024-08-16 12:26:18 +0600

    pattern practice

--- Lecture_02/pattern/p12.cpp
+++ Lecture_02/pattern/p12.cpp
@@ -1,5 +1,5 @@
 /*
-D 
+D
 C D
 B C D
 A B C D
@@ -8,11 +8,21 @@ A B C D
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n, row = 1;
 
     cin >> n;
-    while(n<= row){
-        
+    while(row<=n){
+        int col = 1;
+        char start = 'A' + n - row;
+        while(col<=row){
+            cout << start << " ";
+            start = start + 1;
+            col ++;
+        }
+        cout << endl;
+        row++;
     }
+   
 }
