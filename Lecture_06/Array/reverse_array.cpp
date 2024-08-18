2c5277d8f224829b54d1ec458913967b4b4738e6
readul-islam <developer.readul@gmail.com>
2024-08-18 20:31:04 +0600

    reverse array with differnent method

--- Lecture_06/Array/reverse_array.cpp
+++ Lecture_06/Array/reverse_array.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 using namespace std;
-
+// 1st method
 void reverseArray(int arr[], int size)
 {
     int totalSwap = size / 2;
     for (int i = 0; i < totalSwap; i++)
     {
-        int temp = arr[i];
-        arr[i] = arr[size - 1 - i];
-        arr[size - 1 - i] = temp;
+        // custom code for swap
+        // int temp = arr[i];
+        // arr[i] = arr[size - 1 - i];
+        // arr[size - 1 - i] = temp;
+        // built in function for swap
+        swap(arr[i], arr[size-1-i]);
     }
 }
+// 2cnd method
+void reverseArray1(int arr[], int size)
+{
+   int start = 0;
+   int end = size-1;
+
+   while(start<=0){
+    swap(arr[start] , arr[end]);
+    start++;
+    end--;
+   }
+}
+
+
 
 int main()
 {
     int arr[6] = {1, 2, 3, 4, 5, 6};
     int size = 6;
 
-    reverseArray(arr, size);
+    reverseArray1(arr, size);
 
     for (int i = 0; i < size; i++)
     {
