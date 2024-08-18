e1ef4254effb462da71081d5dbd5d5b4e7d970f2
readul-islam <developer.readul@gmail.com>
2024-08-18 18:01:27 +0600

    switch

---
+++ Lecture_05/howNote.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+// will work letter
+int main(){
+    int n ;
+    cin >> n;
+
+    while(n!=0){
+        switch(n){
+            case 100: 
+            int note = n/100;
+            break;
+        }
+    }
+}
---
+++ Lecture_05/switch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 1;
+    while (true)
+    {
+        switch (n)
+        {
+        case 1:
+            cout << 'P' << endl;
+
+            exit(0); // will terminate loop process
+        }
+    }
+}
