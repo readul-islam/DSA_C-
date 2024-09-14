44177dd806ae9bc1925f80567cea4ec842ecfe7d
readul-islam <developer.readul@gmail.com>
2024-09-14 23:33:04 +0600

    update

--- Lecture_13/char.cpp
+++ Lecture_13/char.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
-
+void printArray(const char arr[]) {
+//    someting
+    for (char c : arr) {
+        std::cout << c;
+    }
+    std::cout << std::endl; // To ensure the output ends with a newline
+}
 // start char 
 int main(){
 
