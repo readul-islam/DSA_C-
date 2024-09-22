cc9f3e45cb29b0c698f0f77aadd95134bc82f83f
readul-islam <developer.readul@gmail.com>
2024-09-22 16:28:27 +0600

    applied toLowercase on chekcPalindrome

--- Lecture_13/checkPalindrome.cpp
+++ Lecture_13/checkPalindrome.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+char toLowercase(char ch)
+{
+    if (ch >= 'a' && ch <= 'z')
+    {
+        return ch;
+    }
+    else
+    {
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
 int length(char name[])
 {
     int count = 0;
@@ -16,7 +29,7 @@ bool checkPalindrome(char name[], int len)
     bool isTrue = true;
     for (int i = 0; i <= len / 2; i++)
     {
-        if (name[i] != name[len - i - 1])
+        if (toLowercase(name[i]) != toLowercase(name[len - i - 1]))
         {
             isTrue = false;
         }
