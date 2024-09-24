1a82825b2985e16a5ec8bbc224d343f3841b84a5
readul-islam <developer.readul@gmail.com>
2024-09-24 12:33:31 +0600

    reverse word in a string

--- Lecture_13/keyDiff.cpp
+++ Lecture_13/keyDiff.cpp
@@ -1,18 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     char name[5] = "akka";
     string name1 = "bkka";
     name[1] = '\0';
-    name1[3]='\0';
-    for(int i = 0 ; i<5; i++){
-        cout << name[i] <<" 1" << endl;
+    name1[3] = '\0';
+    for (int i = 0; i < 5; i++)
+    {
+        cout << name[i] << " 1" << endl;
         cout << "=============" << endl;
     }
 
-    for(int i =0; i< name1.length(); i++){
-        cout << name1[i] <<" 2" << endl;
+    for (int i = 0; i < name1.length(); i++)
+    {
+        cout << name1[i] << " 2" << endl;
     }
     cout << name << endl;
     cout << name1.length() << endl;
--- Lecture_13/tempCodeRunnerFile.cpp
+++ Lecture_13/tempCodeRunnerFile.cpp
@@ -1,2 +1,25 @@
-
+ string s = " the sky is blue ";
+    string temp = "", ans = "";
+    for (int i = 0; i <= s.length(); i++)
+    {
+        if (isalnum(s[i]) == 1)
+        {
+            temp += s[i];
+            cout << temp << endl;
+            cout << "==========" << endl;
+        }
+        else if (isalnum(s[i]) == 0)
+        {
+            if (ans == "")
+            {
+                ans = temp;
+                temp = "";
+            }
+            else
+            {
+                ans = temp + " " + ans;
+                temp = "";
+            }
+        }
     }
+    cout << ans;
