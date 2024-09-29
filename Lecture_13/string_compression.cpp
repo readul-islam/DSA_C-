3fd0fa2dec79d17e842a009e5bcead962cbd7163
readul-islam <developer.readul@gmail.com>
2024-09-29 14:05:12 +0600

    string compression

--- Lecture_13/string_compression.cpp
+++ Lecture_13/string_compression.cpp
@@ -5,46 +5,34 @@ using namespace std;
 
 int main()
 {
-    string ch[8] = {"a", "a", "b", "b", "c", "c", "c"};
-    vector<string> output;
-    int size = 8;
-    int left = 0;
-    int right = 1;
-    while (right < size)
-    {
-        if (ch[left] == ch[right])
-        {
-            right++;
-        }
-        else
-        {
+    vector<char> chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    int len = chars.size();
+    int ansIndex = 0;
+    int i = 0;
 
-            output.push_back(ch[left]);
-            int count = right - left;
-            string s = to_string(count);
-            if (count == 1)
-            {
-                left = right;
-                continue;
-            }
-            else if (count < 10)
-            {
+    while(i<len){
+        int j = i+1;
+        while(j<len && chars[i] == chars[j]){
+            j++;
+        };
 
-                output.push_back(to_string(count));
-            }
-            else
-            {
-                string countStr = to_string(count);
-                for (char digit : countStr)
-                {
-                    output.push_back(string(1, digit)); 
-                }
+        chars[ansIndex++] = chars[i];
+        int count = j - i;
+
+        if(count >1){
+            string cnt = to_string(count);
+            for(char ch :cnt){
+                chars[ansIndex++] = ch;
             }
         }
+
+        i=j;
+
     }
 
+   for(int i = 0; i<len;i++){
+    cout << chars[i] << " ";
+   }
 
-    for(int i = 0; i< output.size(); i++){
-        cout << output[i] << endl;
-    }
+
 }
