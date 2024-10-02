b583fc810f35cab431d179bfbef4a99a6a19937d
readul-islam <developer.readul@gmail.com>
2024-10-02 19:17:32 +0600

    array pointer

--- pointer/p_function.cpp
+++ pointer/p_function.cpp
@@ -4,24 +4,34 @@
 #include <cmath>
 using namespace std;
 
-void print(int *p){
+void print(int *p)
+{
     // (*p)++;
-  *p = *p + 1;
-// cout << p << endl;
-// cout << *p << endl;
+    *p = *p + 1;
+    // cout << p << endl;
+    // cout << *p << endl;
+};
+
+
+int sum (int *arr , int n){
+   int sum = 0;
+    for(int i = 0; i < n; i++){
+    sum+= arr[i];
+    }
+    return sum;
 }
 
-int main() {
+int main()
+{
 
+    /* write code here */
+    int value = 5;
+    int *p = &value;
 
-/* write code here */
-int value = 5; 
-int *p = &value;
-
-cout << "before value " << value << endl;
-print(p);
-cout << "after value " << value << endl;
-
-
+    cout << "before value " << value << endl;
+    print(p);
+    cout << "after value " << value << endl;
+    int arr [5] = {1,22, 33,3,4};
+    cout << sum(arr, 5) << endl; 
     return 0;
 };
