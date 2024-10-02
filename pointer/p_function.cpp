ee123b25639b93e5bed13c4c0711227881dc3701
readul-islam <developer.readul@gmail.com>
2024-10-02 18:56:40 +0600

    update

--- pointer/p_function.cpp
+++ pointer/p_function.cpp
@@ -4,11 +4,23 @@
 #include <cmath>
 using namespace std;
 
+void print(int *p){
+    // (*p)++;
+  *p = *p + 1;
+// cout << p << endl;
+// cout << *p << endl;
+}
 
 int main() {
 
 
 /* write code here */
+int value = 5; 
+int *p = &value;
+
+cout << "before value " << value << endl;
+print(p);
+cout << "after value " << value << endl;
 
 
     return 0;
