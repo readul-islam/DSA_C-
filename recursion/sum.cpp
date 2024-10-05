7543d81cdb73dfb9390bb077952345e5bda9b837
readul-islam <developer.readul@gmail.com>
2024-10-05 14:15:14 +0600

    sum of array using recursion

--- recursion/sum.cpp
+++ recursion/sum.cpp
@@ -9,8 +9,7 @@ int sumArray(int *arr, int size)
     if (size == 0)
         return 0;
 
-    int sum = arr[0] + sumArray(arr + 1, size - 1);
-    return sum;
+    return arr[0] + sumArray(arr + 1, size - 1);
 }
 
 int main()
