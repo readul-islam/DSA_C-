eb0c26d74d2b4503775e9749368492be0dffb0bb
readul-islam <developer.readul@gmail.com>
2024-10-10 19:35:36 +0600

    solved

--- recursion/string_subsequences.cpp
+++ recursion/string_subsequences.cpp
@@ -1,24 +1,26 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<cmath>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
-void subsequences(string txt, int index, vector<string> &ans, string output){
-    if(index == txt.size()){
+void subsequences(string txt, int index, vector<string> &ans, string output)
+{
+    if (index >= txt.size())
+    {
         ans.push_back(output);
         return;
     };
 
-    //exclude 
-    subsequences(txt, index+1, ans, output);
+    // exclude
+    subsequences(txt, index + 1, ans, output);
 
     // include
     char ch = txt[index];
-    
+    output += ch;
+   
 
-    subsequences(txt, index+1, ans, output);
-
+    subsequences(txt, index + 1, ans, output);
 }
 
 int main()
@@ -26,7 +28,12 @@ int main()
     string txt = "abc";
     string output = "";
     vector<string> ans;
-
+    subsequences(txt, 0, ans, output);
+    cout << ans.size() << endl;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
 
     return 0;
 };
