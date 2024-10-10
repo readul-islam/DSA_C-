78464e625974d622c91db1cca41069a4f359431e
readul-islam <developer.readul@gmail.com>
2024-10-10 17:59:37 +0600

    subsequences of string

---
+++ recursion/string_subsequences.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+void subsequences(string txt, int index, vector<string> &ans){
+    if(index == txt.size()){
+        ans.
+        return ;
+    }
+}
+
+int main()
+{
+    string txt = "abc";
+    vector<string> ans;
+
+
+    return 0;
+};
